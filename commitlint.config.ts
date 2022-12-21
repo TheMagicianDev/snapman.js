b530155c6497b0c08cdf51a77b70181f5a0fe162
Mohamed Lamine Allal <allalmohamedlamine@gmail.com>
2022-12-21 20:40:07 +0100

    chore: fixing configuration and adding coverage

--- commitlint.config.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-import-module-exports */
-import type { UserConfig } from '@commitlint/types';
-
-const Configuration: UserConfig = {
-  extends: ['@commitlint/config-conventional'],
-};
-
-exports.module = Configuration;
