2a19e03fb5f68364423a01c02cea97542885bcb4
Mohamed Lamine Allal <allalmohamedlamine@gmail.com>
2022-12-22 00:20:07 +0100

    chore: fix build, use webpack with swc-loader

---
+++ webpack.config.cjs
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const p = (pth) => path.resolve(__dirname, pth);
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist/bundle'),
+    library: 'SnapmanJs',
+  },
+  mode: 'production',
+  devtool: false,
+  target: ['web', 'es5'],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'swc-loader',
+        // use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    extensionAlias: {
+      '.js': ['.ts'],
+    },
+    alias: {
+      '/Snap': p('./src/Snap'),
+      '/Snapman': p('./src/Snapman'),
+      '/Utils': p('./src/Utils'),
+    },
+  },
+};
